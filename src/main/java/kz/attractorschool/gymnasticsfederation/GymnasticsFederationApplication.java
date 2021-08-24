1bd3c8d8ba8ce977f6348df548ebc547321ab9a7
Адылжан Тухтамов <adyl0810@gmail.com>
2021-08-24 14:29:03 +0600

    Initial commit
    Добавлен файл .gitignore
    Проект собран с помощью Maven.
    Подключены библиотеки Spring Web, Spring JPA, Flyway Migration, MySQL driver.

---
+++ src/main/java/kz/attractorschool/gymnasticsfederation/GymnasticsFederationApplication.java
@@ -0,0 +1,13 @@
+package kz.attractorschool.gymnasticsfederation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymnasticsFederationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymnasticsFederationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/attractorschool/gymnasticsfederation/GymnasticsFederationApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.attractorschool.gymnasticsfederation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymnasticsFederationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

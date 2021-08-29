c9523fc07830efa9af42336830a38b3137af530e
Lyalya Aldamzharova <lyalyajm@gmail.com>
2021-08-29 20:57:49 +0600

    Создала сущности разряда спортсмена и дисциплин

---
+++ src/main/java/kz/attractorschool/gymnasticsfederation/model/Discipline.java
@@ -0,0 +1,25 @@
+package kz.attractorschool.gymnasticsfederation.model;
+
+import com.sun.istack.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "disciplines")
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Discipline {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    @NotNull
+    private String name;
+}

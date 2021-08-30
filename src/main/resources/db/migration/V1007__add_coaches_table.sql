da534806e839f9b9906b1f96f55d16991fd2d09a
Lyalya Aldamzharova <lyalyajm@gmail.com>
2021-08-30 17:36:13 +0600

    Исправила название таблицы

--- src/main/resources/db/migration/V1007__add_coaches_table.sql
+++ src/main/resources/db/migration/V1007__add_coaches_table.sql
@@ -11,7 +11,7 @@ CREATE TABLE `coaches`
     `trainer_categories_id` INT NOT NULL ,
     `category_file_id` int NOT NULL references `coach_category_files` (`id`),
     PRIMARY KEY (`id`),
-        CONSTRAINT `fk_trainer_category` FOREIGN KEY (`trainer_categories_id`) REFERENCES `trainer_categories` (`id`),
+        CONSTRAINT `fk_trainer_category` FOREIGN KEY (`trainer_categories_id`) REFERENCES `coaches_categories` (`id`),
         CONSTRAINT `fk_trainer_person` FOREIGN KEY (`person_id`) REFERENCES `persons` (`id`),
         CONSTRAINT `fk_trainer_school` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
         CONSTRAINT `fk_trainer_discipline` FOREIGN KEY (`discipline_id`) REFERENCES `disciplines` (`id`)

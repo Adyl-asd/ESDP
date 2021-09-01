fa4f79308e8a4abb15e7bc3f0d30aa45a7830587
Адылжан Тухтамов <adyl0810@gmail.com>
2021-09-01 12:43:35 +0600

    Добавлена страница просмотра всех дисциплин и фронт для CRUD.

--- src/main/java/kz/attractorschool/gymnasticsfederation/frontend/FrontendController.java
+++ src/main/java/kz/attractorschool/gymnasticsfederation/frontend/FrontendController.java
@@ -1,18 +1,31 @@
 package kz.attractorschool.gymnasticsfederation.frontend;
 
+import kz.attractorschool.gymnasticsfederation.model.Discipline;
+import kz.attractorschool.gymnasticsfederation.service.DisciplineService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping
 @AllArgsConstructor
 public class FrontendController {
 
+    DisciplineService disciplineService;
+
     @GetMapping
     public String index() {
         return "index";
     }
 
+    @GetMapping("/disciplines")
+    public String getDisciplines(Model model) {
+        List<Discipline> disciplines = disciplineService.all();
+        model.addAttribute("disciplines", disciplines);
+        return "disciplines/disciplines";
+    }
 }

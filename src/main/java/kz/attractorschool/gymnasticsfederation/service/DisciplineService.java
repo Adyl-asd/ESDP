9b0a49c59eaf7ebc6223530b6ff41b1d1416bb76
Ринат Изахунов <rinat.izakhunov@gmail.com>
2021-09-03 18:09:58 +0600

    #33 Дисциплина. CRUD 1/1
    Добавлены:
     - DisciplineDTO.javaDTO.java;
     - CRUD методы в DisciplineService.java;
     - обработчики в DisciplineController.java;
     - шаблон представления discipline_update.ftlh.

--- src/main/java/kz/attractorschool/gymnasticsfederation/service/DisciplineService.java
+++ src/main/java/kz/attractorschool/gymnasticsfederation/service/DisciplineService.java
@@ -1,6 +1,7 @@
 package kz.attractorschool.gymnasticsfederation.service;
 
 
+import kz.attractorschool.gymnasticsfederation.dto.DisciplineDTO;
 import kz.attractorschool.gymnasticsfederation.exception.ResourceNotFoundException;
 import kz.attractorschool.gymnasticsfederation.model.Discipline;
 import kz.attractorschool.gymnasticsfederation.repository.DisciplineRepository;
@@ -22,5 +23,30 @@ public class DisciplineService {
         return repository.findById(id).orElseThrow(() -> {
             return new ResourceNotFoundException("Дисциплина", id);
         });
+    }
+
+    public DisciplineDTO getOne(Integer id){
+        return DisciplineDTO.from(findOne(id));
+    }
+
+    public DisciplineDTO add(DisciplineDTO disciplineDTO){
+        Discipline discipline = repository.save(Discipline.builder()
+                .name(disciplineDTO.getName())
+                .build());
+        return DisciplineDTO.from(discipline);
+    }
+
+    public String delete(Integer id){
+        Discipline discipline = findOne(id);
+        discipline.setDel(true);
+        repository.save(discipline);
+        return "ok";
+    }
+
+    public DisciplineDTO update(DisciplineDTO disciplineDTO, Integer id){
+        Discipline discipline = findOne(id);
+        discipline.setName(disciplineDTO.getName());
+        repository.save(discipline);
+        return DisciplineDTO.from(discipline);
     }
 }

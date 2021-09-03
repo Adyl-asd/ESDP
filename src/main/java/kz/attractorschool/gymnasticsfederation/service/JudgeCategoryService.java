005782ad4b91ad9c8000ddeb4c4a9bb831157d33
Ринат Изахунов <rinat.izakhunov@gmail.com>
2021-09-03 18:14:13 +0600

    #36 Категория судьи. CRUD 1/1
    Добавлены:
     - JudgeCategoryDTO.java;
     - CRUD методы в JudgeCategoryService.java;
     - обработчики в JudgeCategoryController.java;
     - шаблоны представления judge_categories.ftlh и judge_category_update.ftlh.

--- src/main/java/kz/attractorschool/gymnasticsfederation/service/JudgeCategoryService.java
+++ src/main/java/kz/attractorschool/gymnasticsfederation/service/JudgeCategoryService.java
@@ -1,6 +1,9 @@
 package kz.attractorschool.gymnasticsfederation.service;
 
+import kz.attractorschool.gymnasticsfederation.dto.CoachCategoryDTO;
+import kz.attractorschool.gymnasticsfederation.dto.JudgeCategoryDTO;
 import kz.attractorschool.gymnasticsfederation.exception.ResourceNotFoundException;
+import kz.attractorschool.gymnasticsfederation.model.CoachCategory;
 import kz.attractorschool.gymnasticsfederation.model.JudgeCategory;
 import kz.attractorschool.gymnasticsfederation.repository.JudgeCategoryRepository;
 import lombok.AllArgsConstructor;
@@ -22,5 +25,30 @@ public class JudgeCategoryService {
         return repository.findById(id).orElseThrow(() -> {
             return new ResourceNotFoundException("Категория судьи", id);
         });
+    }
+
+    public JudgeCategoryDTO getOne(Integer id){
+        return JudgeCategoryDTO.from(findOne(id));
+    }
+
+    public JudgeCategoryDTO add(JudgeCategoryDTO judgeCategoryDTO){
+        JudgeCategory judgeCategory = repository.save(JudgeCategory.builder()
+                .number(judgeCategoryDTO.getNumber())
+                .build());
+        return JudgeCategoryDTO.from(judgeCategory);
+    }
+
+    public String delete(Integer id){
+        JudgeCategory judgeCategory = findOne(id);
+        judgeCategory.setDel(true);
+        repository.save(judgeCategory);
+        return "ok";
+    }
+
+    public JudgeCategoryDTO update(JudgeCategoryDTO judgeCategoryDTO, Integer id){
+        JudgeCategory judgeCategory = findOne(id);
+        judgeCategory.setNumber(judgeCategoryDTO.getNumber());
+        repository.save(judgeCategory);
+        return JudgeCategoryDTO.from(judgeCategory);
     }
 }

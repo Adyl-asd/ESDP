e91ec4dab4c8369295dade5349c86ee2a2ecdb67
Lyalya Aldamzharova <lyalyajm@gmail.com>
2021-09-20 10:07:47 +0600

    Отредактировала миграцию

--- src/main/resources/db/migration/V1014__add_participation_application_table.sql
+++ src/main/resources/db/migration/V1014__add_participation_application_table.sql
@@ -5,24 +5,28 @@ CREATE TABLE `participation_applications`(
     `creation_date` DATE NOT NULL,
     `is_del` BOOLEAN NOT NULL,
     `school_id` INT NOT NULL REFERENCES `schools`(`id`),
-    `competition_id` INT NOT NULL REFERENCES `competitions`(`id`)
+    `competition_id` INT NOT NULL REFERENCES `competitions`(`id`),
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `participation_applications_athletes`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `participation_application_id` INT NOT NULL REFERENCES `participation_applications`(`id`),
     `athlete_id` INT NOT NULL REFERENCES  `athletes`(`id`),
-    `competitions_discipline_age_id` INT NOT NULL REFERENCES `competitions_discipline_ages`(`id`)
+    `competitions_discipline_age_id` INT NOT NULL REFERENCES `competitions_discipline_ages`(`id`),
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `participation_applications_judges`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `participation_application_id` INT NOT NULL REFERENCES `participation_applications`(`id`),
-    `judge_id` INT NOT NULL REFERENCES  `judges`(`id`)
+    `judge_id` INT NOT NULL REFERENCES  `judges`(`id`),
+    PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `participation_applications_coaches`(
     `id` INT NOT NULL AUTO_INCREMENT,
     `participation_application_id` INT NOT NULL REFERENCES `participation_applications`(`id`),
-    `coach_id` INT NOT NULL REFERENCES  `coaches`(`id`)
+    `coach_id` INT NOT NULL REFERENCES  `coaches`(`id`),
+    PRIMARY KEY (`id`)
 );
